9861f399d6fcfe47e6c0c1e51476786cbf07b587
sibeldemirel <sibeldmrl19@gmail.com>
2024-01-02 16:45:43 +0100

    ex1 optimized

--- src/test/java/exercices/MoyenneTest.java
+++ src/test/java/exercices/MoyenneTest.java
@@ -1,29 +1,33 @@
 package exercices;
 
 import exercices.TP1.moyenne.Moyenne;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MoyenneTest {
+    private Moyenne resultatCalcul;
+    @BeforeEach
+    public void mesMoyennes(){
+        resultatCalcul = new Moyenne();
+    }
+
     @Test
     public void testMoyenne() {
         int[] notes = new int[]{12, 6, 15};
-        Moyenne resultatCalcul = new Moyenne();
         Integer result = resultatCalcul.moyenne(notes);
         assertEquals(11, result, "il y a une erreur de calcul");
     }
     @Test
     public void testNoteInccorecte(){
         int[] notes = new int[]{32, 6, 24};
-        Moyenne resultatCalcul = new Moyenne();
         assertThrows(IllegalArgumentException.class, () -> resultatCalcul.moyenne(notes));
     }
 
     @Test
     public void testNoteNb() {
         int[] notes = new int[]{11, 6};
-        Moyenne resultatCalcul = new Moyenne();
         assertThrows(IllegalArgumentException.class, () -> resultatCalcul.moyenne(notes));
     }
 }
