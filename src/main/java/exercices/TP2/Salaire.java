ab0c258081817fd913b6e5e6b3dd59d41da3ae71
sibeldemirel <sibeldmrl19@gmail.com>
2024-01-04 10:07:54 +0100

    class salaire  :beers:

--- src/main/java/exercices/TP2/Salaire.java
+++ src/main/java/exercices/TP2/Salaire.java
@@ -7,7 +7,22 @@ public class Salaire {
         this.tauxHoraire = tauxHoraire;
     }
 
-    public void payer(int heures){
+    /** Calcul du salaire normal et avec heures supp (heures min de travail = 151,67)
+     * @param heures (int)
+     * @return salaireTotal (int)
+     */
+    public int payer(int heures){
+        int heuresNormales = (int) Math.min(heures, 151.67);
+        int heuresSupplementaires = (int) Math.max(heures - 151.67, 0);
 
+        // Calcul du salaire pour les heures normales
+        int salaireNormal = heuresNormales * tauxHoraire;
+
+        // Calcul du salaire pour les heures supplémentaires à un taux majoré de 25%
+        double tauxSupplementaire = 1.25;
+        int salaireSupplementaire = (int) (heuresSupplementaires * tauxHoraire * tauxSupplementaire);
+
+        // Calcul du salaire total
+        return salaireNormal + salaireSupplementaire;
     }
 }
