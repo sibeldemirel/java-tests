80c4e8a27a40c5ca2913f674361b4e7a426d2bc6
sibeldemirel <sibeldmrl19@gmail.com>
2024-01-04 16:27:42 +0100

    rm comments

--- src/test/java/exercices/TP2/SalaireTest.java
+++ src/test/java/exercices/TP2/SalaireTest.java
@@ -1,6 +1,5 @@
 package exercices.TP2;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -12,12 +11,11 @@ class SalaireTest {
     @CsvSource({"10"})
     public void payer( int tauxHoraire){
         Salaire salaire = new Salaire(tauxHoraire);
-        double salaireNormal = salaire.payer(140); // 140 heures travaillées
-        assertEquals(1400, salaireNormal); // 140 heures * 10 (taux horaire) = 1400 euros
+        double salaireNormal = salaire.payer(140);
+        assertEquals(1400, salaireNormal);
 
-        // Test pour les heures supplémentaires
-        double salaireSupplementaire = salaire.payer(160); // 160 heures travaillées
-        assertEquals(1622, salaireSupplementaire); // 151.67 heures * 10 + 25% * (160 - 151.67) * 10 = 1622 euros
+        double salaireSupplementaire = salaire.payer(160);
+        assertEquals(1622, salaireSupplementaire);
     }
 
 }
