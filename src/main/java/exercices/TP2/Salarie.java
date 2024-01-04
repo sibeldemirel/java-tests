a62e81c6f43fc2a6b3c02ab42c1e9d6d0691c78d
sibeldemirel <sibeldmrl19@gmail.com>
2024-01-04 10:22:11 +0100

    class salarie :beers:

--- src/main/java/exercices/TP2/Salarie.java
+++ src/main/java/exercices/TP2/Salarie.java
@@ -7,7 +7,18 @@ public abstract class Salarie {
 
     public abstract void demenager(Adresse adresse);
 
+    /** fait travailler le salarie un certain nombre d'heures
+     * affiche String indiquant le salaire
+     * @param heures (int)
+     */
     public void travailler(int heures) {
+        if (heures <= 0) {
+            heures = 151; // Si aucune heure n'est spécifiée, on considère 151.67 heures
+        }
+
+        double salaireGagne = salaire.payer(heures);
+
+        System.out.println("Le salarié Jean Dupont a travaillé " + heures + " heures et a perçu " + salaireGagne + " euros");
     }
 
     public abstract void augmenter(double pourcentage);
