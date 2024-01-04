a4eea450c688e1ab771f1b626ba6bf8ba64b6cb1
sibeldemirel <sibeldmrl19@gmail.com>
2024-01-04 10:10:20 +0100

    class salarieCDI  :beers:

--- src/main/java/exercices/TP2/SalaireCdi.java
+++ src/main/java/exercices/TP2/SalaireCdi.java
@@ -1,5 +1,14 @@
 package exercices.TP2;
 
-public class SalaireCdi {
+public class SalaireCdi extends Salarie{
 
+    @Override
+    public void demenager(Adresse adresse) {
+
+    }
+
+    @Override
+    public void augmenter(double pourcentage) {
+
+    }
 }
