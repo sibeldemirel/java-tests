6f256d69a8a5b2435d44cab0144dbad4e39b5a76
sibeldemirel <sibeldmrl19@gmail.com>
2024-01-05 14:57:55 +0100

    test(thermometre): empty table failed

--- src/test/java/exercices/TP3/thermometre/ThermometreTest.java
+++ src/test/java/exercices/TP3/thermometre/ThermometreTest.java
@@ -1,7 +1,22 @@
 package exercices.TP3.thermometre;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ThermometreTest {
+    private Thermometre thermometre;
 
+    @BeforeEach
+    public void setUp(){
+        thermometre = new Thermometre();
+    }
+
+    @Test
+    public void test_if_empty_table(){
+        int[] tab = new int[4];
+        int[] expected = new int[0];
+        assertEquals(expected, tab);
+    }
 }
